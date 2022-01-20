1cc44aea108defd238631351c713a21799d370cc
Colin <colin.panter.1@ulaval.ca>
2022-01-20 11:45:41 -0500

    Added no results case

--- lab2/js/scripts.js
+++ lab2/js/scripts.js
@@ -1,12 +1,17 @@
 var elements = "Lorem ipsum dolor sit amet consectetur adipiscing elit Proin quis nibh arcu Donec rhoncus a".split(" ")
 
 function search() {
+    var foundMatches = false;
     document.getElementById("elements").innerHTML = ""
     elements.forEach(element => {
         if (element.includes(document.getElementById("dropinput").value)) {
-            document.getElementById("elements").innerHTML += "<p onclick=\"choose('"+element+"')\">"+element+"</p>";
+            foundMatches = true;
+            document.getElementById("elements").innerHTML += "<p class=\"element\" onclick=\"choose('"+element+"')\">"+element+"</p>";
         }
     });
+    if (!foundMatches) {
+        document.getElementById("elements").innerHTML += "<p class=\"noresults\"><i>No results found</i></p>";
+    }
 }
 
 function choose(s) {
