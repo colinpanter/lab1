184ba80d961ae8f925e531cfeeee4f3674d117a2
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-01-26 03:33:46 -0500

    Toaster comming along

--- lab3/src/scripts.js
+++ lab3/src/scripts.js
@@ -1,3 +1,43 @@
-export function toast() {
-    alert(`Hello!`);
+export function toaster(type) {
+    const toast = new Toast(type, document.getElementById("input").value);
+    toast.showToast();
   }
+
+class Toast{
+  constructor(type, message){
+    this.type = type;
+    this.message = message;
+  }
+
+  showToast() {
+    let toast = document.createElement("div");
+    let biggerToast = document.createElement("div");
+    let title = document.createElement("p");
+    let text = document.createElement("p");
+    let img = document.createElement("img");
+    
+    if(this.type === "Warning"){
+      img.src = ""
+    } else if(this.type === "Success"){
+      img.src = ""
+    } else if(this.type === "Info"){
+      img.src = ""
+    }
+
+    title.innerHTML = this.type;
+    text.innerHTML = this.message;
+    toast.className = "toast";
+    biggerToast.className = "biggerToast";
+
+    biggerToast.appendChild(toast);
+    toast.appendChild(img);
+    toast.appendChild(title);
+    toast.appendChild(text);
+
+    biggerToast.onclick = function(){ 
+      biggerToast.remove();
+     };
+
+    document.getElementById("plate").appendChild(biggerToast);
+  }
+}
