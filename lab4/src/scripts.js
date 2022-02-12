ab1a7710ba0977f1eb2e92c99f31f6772a3e5a9e
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-02-11 21:15:01 -0500

    a peine commencer

--- lab4/src/scripts.js
+++ lab4/src/scripts.js
@@ -1,3 +1,18 @@
+const request = new XMLHttpRequest();
+const url = 'https://glo3102lab4.herokuapp.com/';
+const id = "c6ac7c1d-6eac-4455-abbf-3252f3c29163";
+const fullUrl = url + id;
+request.open("GET", fullUrl + "/tasks");
+request.send();
+
+request.onload = (e) => {
+    alert(request.response);
+}
+
+function getTasks(){
+    let tasks = request.open("GET", fullUrl + "/tasks");
+}
+
 export default function load() {
 
 }
