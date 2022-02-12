8561d28a62454e252f0b91b60a09c564a5f01587
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-02-12 00:58:43 -0500

    Tp4 "marche"

--- lab4/src/scripts.js
+++ lab4/src/scripts.js
@@ -1,26 +1,70 @@
-const request = new XMLHttpRequest();
 const url = 'https://glo3102lab4.herokuapp.com/';
-const id = "c6ac7c1d-6eac-4455-abbf-3252f3c29163";
+const id = "c38f5712-c21a-47a7-8669-e6ebd7471ba4";
 const fullUrl = url + id + "/tasks";
-request.open("GET", fullUrl);
-request.send();
 
-request.onload = (e) => {
-    // alert(request.response);
+export const sendTasks = async function(){
+    const sendRequest = await fetch(fullUrl, {
+        method:"post",
+        body:JSON.stringify({name:document.getElementById("input").value}),
+        headers:new Headers({"Content-Type": "application/json"})
+    });
+    const jsonResponse = await sendRequest.json();
+    getTasks();
 }
 
-function getTasks(){
-    let tasks = request.open("GET", fullUrl + "/tasks");
+export const load = async function(){
+    const response = await fetch(fullUrl);
+    const jsonResponse = await response.json();
 }
 
-function sendTasks(){
-    let textToSend = document.getElementById("input");
+export const getTasks = async function(){
+    const response = await fetch(fullUrl);
+    const jsonResponse = await response.json();
+    const fuckYouColin = document.getElementById("tasksPlate");
+    fuckYouColin.innerHTML = "";
 
+    for(let i=0; i < jsonResponse.tasks.length ; i++){
+        let task = jsonResponse.tasks[i];
 
-    request.open("POST", fullUrl);
-    request.send(textToSend);
+        let divTask = document.createElement("div");
+        let text = document.createElement("p");
+        let buttonDelete = document.createElement("button");
+        let buttonEdit = document.createElement("button");
+        let newInput = document.createElement("input");
+
+        newInput.className = "newInput"
+        divTask.className = "divTask";
+        text.innerHTML = task.name;
+        buttonDelete.innerHTML = "DELETE";
+        buttonEdit.innerHTML = "EDIT";
+        buttonDelete.onclick = function(){deleteTask(task.id)};
+        buttonEdit.onclick = function(){
+            newInput.placeholder = task.name;
+            newInput.onkeydown = function(){clickPress(event, task.id, newInput.value)};
+            text.innerHTML = "";
+            divTask.appendChild(newInput);
+        }
+
+        divTask.appendChild(text);
+        divTask.appendChild(buttonEdit);
+        divTask.appendChild(buttonDelete);
+
+        fuckYouColin.appendChild(divTask);
+    }
 }
 
-export default function load() {
+const deleteTask = async function(taskID){
+    const deleteRequest = await fetch(fullUrl + "/" + taskID, {method:"delete"});
+    getTasks();
+}
 
+const clickPress = async function(event, taskID, test) {
+    if (event.keyCode == 13) {
+        const putRequest = await fetch(fullUrl + "/" + taskID, {
+            method:"put",
+            body:JSON.stringify({name:test}),
+            headers:new Headers({"Content-Type": "application/json"})
+        });
+        getTasks();
+    }
 }
