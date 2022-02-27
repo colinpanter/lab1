4d5de537e6231f252182544d2e4e9443b6610bc4
Colin <colin.panter.1@ulaval.ca>
2022-02-27 03:17:11 -0500

    Started lab5

---
+++ lab5/src/App.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: "#page",
+    data: {
+        inputValue: "",
+        todos: []
+    },
+    methods: {
+        async createTodo() {},
+        async updateTodo() {},
+        async deleteTodo() {}
+    },
+    beforeCreate(){},
+    async mounted(){}
+})
