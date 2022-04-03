192539d2c7a85a7be8be2f1aeec9d1e83cdd3699
Colin <colin.panter.1@ulaval.ca>
2022-04-03 18:37:58 -0400

    Fixed css

--- lab9/lab9.js
+++ lab9/lab9.js
@@ -6,7 +6,7 @@ const server = http.createServer(app)
 const {Server} = require('socket.io')
 const io = new Server(server)
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/css'));
 
 
 app.get('/', (req, res) => {
